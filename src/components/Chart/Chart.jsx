0db102baa6318cb8e0351fe5b9ea018f7abb7f6b
Kenji <kenjiyasumitsu@gmail.com>
2021-09-28 21:48:56 +0900

    add expensesChart component

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,23 @@
+import './Chart.css';
+
+import ChartBar from './ChartBar';
+
+const Chart = ({ dataPoints }) => {
+	const dataPointsValues = dataPoints.map((dataPoint) => dataPoint.value);
+	const totalMaximum = Math.max(...dataPointsValues);
+
+	return (
+		<div className="chart">
+			{dataPoints.map((dataPoint) => (
+				<ChartBar
+					key={dataPoint.label}
+					value={dataPoint.value}
+					maxValue={totalMaximum}
+					label={dataPoint.label}
+				/>
+			))}
+		</div>
+	);
+};
+
+export default Chart;
